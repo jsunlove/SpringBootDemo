d000a9b5839dafd0b7cc339659eb3ba0c859734d
jsunlove <972459714@qq.com>
2019-12-12 18:00:39 +0800

    Initial commit

---
+++ src/main/java/com/jsun/JsunApplication.java
@@ -0,0 +1,13 @@
+package com.jsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsun/JsunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
