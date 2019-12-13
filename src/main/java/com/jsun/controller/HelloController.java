a193db6eb02bd3e4010fa428fa03bded53da5319
jsunlove <972459714@qq.com>
2019-12-13 18:21:39 +0800

    看到p8

--- src/main/java/com/jsun/controller/HelloController.java
+++ src/main/java/com/jsun/controller/HelloController.java
@@ -1,13 +1,11 @@
 package com.jsun.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
